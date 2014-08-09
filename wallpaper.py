ddb15d3094047d10e5a11426e818809df6fc0ce6
Adithya Srinivasan <a4sriniv@uwaterloo.ca>
2014-08-09 00:22:48 -0700

    Some code formatting

--- wallpaper.py
+++ wallpaper.py
@@ -7,11 +7,12 @@ import re
 import shutil
 import logging
 
+
 class ArsenalWallpaperExtractor(object):
-    
     def __init__(self):
         self.file = ""
-        self.arsenal_wallpapers_link = "http://www.arsenal.com/fanzone/wallpapers"
+        self.arsenal_wallpapers_link = \
+            "http://www.arsenal.com/fanzone/wallpapers"
         self.wallpaper_directory = self._get_wallpaper_directory()
         self.latest_picture_number = self._get_latest_picture_number()
 
@@ -25,12 +26,13 @@ class ArsenalWallpaperExtractor(object):
         files = os.listdir(self.wallpaper_directory)
         logging.info('Current files in wallpaper directory: ' + str(files))
         picture_numbers = []
-        for filename in files: # TODO: try and make this a list comprehension
+        for filename in files:  # TODO: try and make this a list comprehension
             m = re.search(r'gun__(\d+)_3.jpg', filename)
             if m:
                 picture_numbers.append(m.group(1))
         if not picture_numbers:
-            return 0 # TODO: Still need to fix bugs for directory with no new pictures
+            return 0  # TODO: Still need to fix bugs for directory with no
+            # new pictures
         picture_numbers.sort(reverse=True)
         return picture_numbers[0]
 
@@ -42,27 +44,31 @@ class ArsenalWallpaperExtractor(object):
         os.remove(os.path.join(os.getcwd(), self.file))
 
     def _sort_by_picture_number(self, date_and_number_tuples):
-        return sorted(date_and_number_tuples, key=lambda pair: pair[1], reverse=True)
-    
+        return sorted(date_and_number_tuples, key=lambda pair: pair[1],
+                      reverse=True)
+
     def _parse_html_file(self):
         with open(self.file, 'r') as f:
             file_contents = f.read()
         regex = '/assets/_files/desktops/(\w+_\d+)/gun__(\d+)_3.jpg'
-        date_and_number_tuples = self._sort_by_picture_number(re.findall(regex, file_contents))
+        date_and_number_tuples = self._sort_by_picture_number(
+            re.findall(regex, file_contents))
         return date_and_number_tuples
 
 
     def _get_newer_pictures(self, date_and_number_tuples):
         if date_and_number_tuples[0][1] <= self.latest_picture_number:
-            print('No new pictures found!')
+            print 'No new pictures found!'
             return
         picture_count = 0
         for date_and_number_tuple in date_and_number_tuples:
             if date_and_number_tuple[1] > self.latest_picture_number:
-                url = 'http://www.arsenal.com/assets/_files/desktops/%s/gun__%s_3.jpg' % (date_and_number_tuple)
+                url = 'http://www.arsenal.com/assets/_files/desktops/%s/gun__' \
+                      '%s_3.jpg' % (date_and_number_tuple)
                 logging.info('Fetching image from url %s', url)
                 picture_name = wget.download(url, bar=None)
-                logging.info('Copying picture %s to %s ....', picture_name, self.wallpaper_directory)
+                logging.info('Copying picture %s to %s ....', picture_name,
+                             self.wallpaper_directory)
                 if self._not_current_directory():
                     shutil.copy(picture_name, self.wallpaper_directory)
                     os.remove(os.path.join(os.getcwd(), picture_name))
@@ -79,20 +85,25 @@ class ArsenalWallpaperExtractor(object):
         self._get_newer_pictures(self._parse_html_file())
         self._delete_html_file()
 
+
 def get_new_wallpapers():
     extractor = ArsenalWallpaperExtractor()
     extractor._extract_wallpapers()
 
+
 def main():
-    print 'Extracting wallpapers from arsenal.com .....' 
+    print 'Extracting wallpapers from arsenal.com .....'
     get_new_wallpapers()
 
 # set logging level and log file
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', filename='wallpaper.log', level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    filename='wallpaper.log', level=logging.DEBUG)
 
 # parse command line arguments
-parser = argparse.ArgumentParser(description='Tool to obtain new arsenal.com wallpapers')
-parser.add_argument('-d', '--dir',  required=True, help='Directory in which wallpapers will be downloaded to.')
+parser = argparse.ArgumentParser(
+    description='Tool to obtain new arsenal.com wallpapers')
+parser.add_argument('-d', '--dir', required=True,
+                    help='Directory in which wallpapers will be downloaded to.')
 args = parser.parse_args()
 
 if __name__ == "__main__":
